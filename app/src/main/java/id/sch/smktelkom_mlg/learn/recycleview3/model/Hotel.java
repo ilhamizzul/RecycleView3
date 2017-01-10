7befc8ca3d71e100789a2d3114c8c2d1f1ce3467
Ilham Izzul <ilhamizzul@gmail.com>
2017-01-10 08:12:52 +0700

    RecyclerView Add

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recycleview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recycleview3/model/Hotel.java
@@ -8,17 +8,17 @@ import java.io.Serializable;
 public class Hotel implements Serializable {
     public String judul;
     public String deskripsi;
-    public String foto;
     public String detail;
     public String lokasi;
+    public String foto;
 
     public Hotel(String judul, String deskripsi, String detail, String lokasi,
                  String foto) {
         this.judul = judul;
         this.deskripsi = deskripsi;
-        this.foto = foto;
         this.detail = detail;
         this.lokasi = lokasi;
+        this.foto = foto;
 
     }
 }
